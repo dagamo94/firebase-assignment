d492dfe86eadf131627142842cb37ac5d0ca2465
Surface <da.ga.mo.94@gmail.com>
2019-06-15 13:43:42 -0600

    Program is finished, might just change visual appearance and time display format later on

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -39,7 +39,9 @@ $("#capture-train-info").on("click", function (event) {
             dbTrainName: trainName,
             dbDestination: destination,
             dbFirstTrainTime: firstTrainTime,
-            dbFrequency: frequency
+            dbFrequency: frequency,
+            dbNextTrain: "",
+            dbMinutesAway: ""
         };
 
         database.ref().push(trainInfoObj);
@@ -49,24 +51,55 @@ $("#capture-train-info").on("click", function (event) {
 });
 
 database.ref().on("child_added", function (childSnapshot) {
+
+    var tFrequency = childSnapshot.val().dbFrequency;
+
+    // Time is 3:30 AM
+    var firstTime = childSnapshot.val().dbFirstTrainTime;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log("FIRST TIME COONVERTED: ",  firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
     var tblRow = $("<tr>");
     var tblRowHead = $("<th>").attr("scope", "row").text(childSnapshot.val().dbTrainName);
     var tblDest = $("<td>").text(childSnapshot.val().dbDestination);
     var tblFreq = $("<td>").text(childSnapshot.val().dbFrequency);
-    var tblNextArrival = $("<td>").text("TBD");
-    var tblMinsAway = $("<td>").text("TBD");
+    var tblNextArrival = $("<td>").text(nextTrain);
+    var tblMinsAway = $("<td>").text(tMinutesTillTrain);
     var removeBtn = $("<td>").append($("<button>").attr({
         type: "submit",
         class: "btn btn-secondary remove",
         id: childSnapshot.val().dbTrainName + "-remove"
     }).text("Remove"));
+    
 
     tblRow.append(tblRow, tblRowHead, tblDest, tblFreq, tblNextArrival, tblMinsAway, removeBtn);
 
     $("#train-schedules").append(tblRow);
 
-    console.log(childSnapshot.val().dbTrainName);
-    console.log(childSnapshot.val().dbDestination);
-    console.log(childSnapshot.val().dbFirstTrainTime);
-    console.log(childSnapshot.val().dbFrequency);
+    // console.log(childSnapshot.val().dbTrainName);
+    // console.log(childSnapshot.val().dbDestination);
+    // console.log(childSnapshot.val().dbFirstTrainTime);
+    // console.log(childSnapshot.val().dbFrequency);
 });
